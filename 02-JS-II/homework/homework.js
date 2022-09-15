c855b28195699089e52f6d4ac82a6e4fc5e81621
Mauricio Araque <araquester@gmail.com>
2022-09-14 23:18:32 -0500

    Tarea resuelta el 14/09/2022

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -222,7 +222,7 @@ function devolverString(str) {
     //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
     //como parámetro un número de euros y calcule el cambio en dólares.
     //Escribe tu código aquí
-    var dolares = euro*1.20;
+    var dolares = euro*1.20; 
     return dolares
   }
   
