13991dbdaad419405e143c7c7a2f8bbbebc7c864
Mauricio Araque <111613206+araquester@users.noreply.github.com>
2022-09-14 20:40:37 -0500

    ejercicios clase 2

---
+++ 02-JS-II/homework/DemoJS-II.js
@@ -0,0 +1,18 @@
+function logHola(){
+	console.log('Hola');
+} 
+
+function logHola2(nombre){
+	console.log('Hola, ' + nombre);
+} 
+
+function logHola3(nombre){
+	console.log('Hola, ',nombre);
+} 
+var miNombre = 'Antonio';
+
+logHola3(miNombre);
+
+logHola2('Martin');
+
+logHola();
